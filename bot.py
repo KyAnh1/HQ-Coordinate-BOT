e886f007349f484e58f491e233719dbd26c0a2af
KyAnh1 <80128970+KyAnh1@users.noreply.github.com>
2022-04-17 02:45:36 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,15 @@
 import os
-import discord
+from dotenv.main import load_dotenv
+from discord.ext import commands
+import time
 
 def main():
-  token = os.getenv("DISCORD_TOKEN")
+  client = discord.Client()
   
+  client.event
+  async def on_ready():
+    print("{0.user} Has connected to Discord".format(client))
+  
+  client.run(os.getenv("DISCORD_TOKEN"))
 if __name__ == '__main__'
   main()
